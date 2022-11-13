88b78d354fcc3bd3787047c9723ae8869c372b9b
jt01052552295 <jt01052552295@gmail.com>
2022-11-13 11:52:44 +0900

    test

--- frontend/components/common/Header.js
+++ frontend/components/common/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Header = () => {
-  return <header className="head">Header</header>;
+  return <header className="head">Header11111</header>;
 };
 
 Header.propTypes = {};
